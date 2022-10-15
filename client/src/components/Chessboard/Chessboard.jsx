d6ef27d75d8db4a3bbde47e7d26905206fd807a9
Ashleen <2552805d@student.gla.ac.uk>
2022-10-15 17:06:49 +0100

    started movePiece function, not working

--- client/src/components/Chessboard/Chessboard.jsx
+++ client/src/components/Chessboard/Chessboard.jsx
@@ -1,10 +1,13 @@
 import Tile from "../Tile/tile";
 import "./Chessboard.css";
 
+
 const verticalAxis = ["1", "2", "3", "4", "5", "6", "7", "8"];
 const horizontalAxis = ["a", "b", "c", "d", "e", "f", "g", "h"];
+const horizontalDic = {"a":1, "b":2, "c":3, "d":4, "e":5, "f":6, "g":7, "h":8};
 
 interface Piece {
+  name: string;
   image: string;
   x: number;
   y: number;
@@ -12,29 +15,63 @@ interface Piece {
 
 const pieces: Piece[] = [];
 
+// for (let p = 0; p < 2; p++) {
+//   const type = p === 0 ? "b" : "w";
+//   const y = p === 0 ? 7 : 0;
+
+//   pieces.push({ name: "Rook", image: `assets/images/rook_${type}.png`, x: 0, y });
+//   pieces.push({ name: "Rook", image: `assets/images/rook_${type}.png`, x: 7, y });
+//   pieces.push({ name: "Knight", image: `assets/images/knight_${type}.png`, x: 1, y });
+//   pieces.push({ name: "Knight", image: `assets/images/knight_${type}.png`, x: 6, y });
+//   pieces.push({ name: "Bishop", image: `assets/images/bishop_${type}.png`, x: 2, y });
+//   pieces.push({ name: "Bishop", image: `assets/images/bishop_${type}.png`, x: 5, y });
+//   pieces.push({ name: "Queen", image: `assets/images/queen_${type}.png`, x: 3, y });
+//   pieces.push({ name: "King", image: `assets/images/king_${type}.png`, x: 4, y });
+
 for (let p = 0; p < 2; p++) {
   const type = p === 0 ? "b" : "w";
-  const y = p === 0 ? 7 : 0;
+  const y = p === 0 ? 8 : 1;
 
-  pieces.push({ image: `assets/images/rook_${type}.png`, x: 0, y });
-  pieces.push({ image: `assets/images/rook_${type}.png`, x: 7, y });
-  pieces.push({ image: `assets/images/knight_${type}.png`, x: 1, y });
-  pieces.push({ image: `assets/images/knight_${type}.png`, x: 6, y });
-  pieces.push({ image: `assets/images/bishop_${type}.png`, x: 2, y });
-  pieces.push({ image: `assets/images/bishop_${type}.png`, x: 5, y });
-  pieces.push({ image: `assets/images/queen_${type}.png`, x: 3, y });
-  pieces.push({ image: `assets/images/king_${type}.png`, x: 4, y });
+  pieces.push({ name: "Rook", image: `assets/images/rook_${type}.png`, x: 0, y });
+  pieces.push({ name: "Rook", image: `assets/images/rook_${type}.png`, x: 7, y });
+  pieces.push({ name: "Knight", image: `assets/images/knight_${type}.png`, x: 1, y });
+  pieces.push({ name: "Knight", image: `assets/images/knight_${type}.png`, x: 6, y });
+  pieces.push({ name: "Bishop", image: `assets/images/bishop_${type}.png`, x: 2, y });
+  pieces.push({ name: "Bishop", image: `assets/images/bishop_${type}.png`, x: 5, y });
+  pieces.push({ name: "Queen", image: `assets/images/queen_${type}.png`, x: 3, y });
+  pieces.push({ name: "King", image: `assets/images/king_${type}.png`, x: 4, y });
 }
 
 for (let i = 0; i < 8; i++) {
-  pieces.push({ image: "assets/images/pawn_b.png", x: i, y: 6 });
+  pieces.push({ name: "Pawn", image: "assets/images/pawn_b.png", x: i, y: 6 });
 }
 
 for (let i = 0; i < 8; i++) {
-  pieces.push({ image: "assets/images/pawn_w.png", x: i, y: 1 });
+  pieces.push({ name: "Pawn", image: "assets/images/pawn_w.png", x: i, y: 1 });
 }
 
-export default function Chessboard() {
+function movePiece(currentPosition, newPosition) {
+    console.log({pieces})
+
+    const current_x = horizontalDic[currentPosition[0]];
+    const current_y = parseInt(currentPosition[1]);
+    const new_x = horizontalDic[newPosition[0]];
+    const new_y = parseInt(newPosition[1]);
+
+    for (let i = 0; i < pieces.length; i ++){
+        if (pieces[i].y === current_y && pieces[i].x === current_x) {
+            pieces[i].y = new_y;
+            pieces[i].x = new_x;
+        }
+    }
+    console.log({pieces})
+
+
+
+
+}
+
+ const Chessboard = () => {
   let board = [];
 
   for (let j = verticalAxis.length - 1; j >= 0; j--) {
@@ -47,10 +84,15 @@ export default function Chessboard() {
           image = p.image;
         }
       });
-
-      board.push(<Tile image={image} number={number} />);
+      board.push(<Tile key={`${j},${i}`} image={image} number={number} />);
     }
   }
 
-  return <div id="chessboard">{board}</div>;
+  return (
+  <div>
+  <div id="chessboard">{board}</div>
+  <button onClick={()=>{movePiece("e2","e4")}}> Change position </button>
+  </div>);
 }
+
+export default Chessboard;
