e5fefab60ce50197fb3e8c38144edbb0230dab15
unknown <petroskitazos@gmail.com>
2022-10-16 13:43:06 +0100

    removed unused directories

--- functions/position_to_words.py
+++
@@ -1,8 +0,0 @@
-
-
-def position_to_words(pos):
-    phonetic_letters = {'a': 'a', 'b': 'bee', 'c': 'cee', 'd': 'dee',
-                        'e': 'e', 'f': 'ef', 'g': 'gee', 'h': 'aitch', }
-    numbers_spelling = {'1': 'one', '2': 'two', '3': 'three',
-                        '4': 'four', '5': 'five', '6': 'six',
-                        '7': 'seven', '8': 'eight'}
